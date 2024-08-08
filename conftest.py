6eddabce5e4df1157dafe5aa48e87b7eab8269c7
KodGlobal <120708212+KodGlobal@users.noreply.github.com>
2024-08-08 12:46:43 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,3 +1,3 @@
 def pytest_report_teststatus(report):
     if report.when == 'call' and report.passed:
-        return "custom_message", "C", "Задание выполнено"
+        return "custom_message", "C", "Task complete"
